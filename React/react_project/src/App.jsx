43b35def9dfbe4cdda6b916775a2e9255659fd8c
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-03-07 14:55:20 +0000

    .

--- React/react_project/src/App.jsx
+++ React/react_project/src/App.jsx
@@ -1,44 +1,42 @@
-function FrontComp(props) {
-  return(<>
-    <li><a href='/' onClick={()=>{
-      props.onMyEvent1();
-    }}>프론트엔드</a></li>
-    <ul>
-      <li>HTML5</li>
-      <li>CSS3</li>
-      <li>Javascript</li>
-      <li>jQuery</li>
-    </ul>
-  </>)
-}
-
-const BackComp = ({onMyEvent2}) =>{
-  return(<>
-    <li><a href="/" onClick={(event)=>{
-      event.preventDefault();
-      onMyEvent2('백엔드 클릭됨(자식전달)');
-    }}>백엔드</a></li>
-    <ul>
-      <li>Java</li>
-      <li>Oracle</li>
-      <li>JSP</li>
-      <li>Sprint Boot</li>
-    </ul>
-  </>)
-}
+import { useState } from 'react';
+import FrontComp from "./components/FrontComp";
+import BackComp from "./components/BackComp";
 
 function App() {
-  return(<>
-    <h2>React-Event</h2>
-    <ol>
-      <FrontComp onMyEvent1={()=>{
-        alert('프론트엔드 클릭됨(부모전달)');
+  const [mode, setMode] = useState('both');
+  const handleSetMode = (mode) => {
+    setMode(mode);
+  };
+  let contents = '';
+  if (mode === 'front'){
+    contents = <>
+      <FrontComp onSetMode={(mode)=>{
+        setMode(mode);
       }}></FrontComp>
-      <BackComp onMyEvent2={(msg)=>{
-        alert(msg)
-      }}/>
+    </>
+  }
+  else if(mode === 'back'){
+    contents = <>
+      <BackComp onSetMode={setMode}/>
+    </>
+  }
+  else{
+    contents = <>
+      <FrontComp onSetMode={(mode)=>{
+        handleSetMode(mode);
+      }}></FrontComp>
+      <BackComp setMode={handleSetMode} />
+    </>
+  }
+  return(<>
+    <h2><a href="/" onClick={(event)=>{
+      event.preventDefault();
+      setMode('both');
+    }}>React-Modules</a></h2>
+    <ol>
+      {contents}
     </ol>
   </>)
 }
 
-export default App
+export default App;
