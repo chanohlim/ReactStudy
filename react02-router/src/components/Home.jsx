cfaea9d785e09ac305bfb5a7fdd91540ae60bead
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-03-15 09:24:22 +0000

    .

---
+++ react02-router/src/components/Home.jsx
@@ -0,0 +1,12 @@
+const Home = () => {
+    return(<>
+        <h2>
+            React Home
+        </h2>
+        <p>
+            React Router에 대해 학습합니다.
+        </p>
+    </>);
+}
+
+export default Home;
