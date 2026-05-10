e74d0933784f388a1c9144f38f7f513aa8fb1da7
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-05-10 13:10:09 +0000

    .

--- react02-router/src/App.jsx
+++ react02-router/src/App.jsx
@@ -1,8 +1,17 @@
-<Routes>
-  <Route path='경로A' element={<컴포넌트1 />} />
-  <Route path='경로B'>
-    <Route index element={<컴포넌트2 />} />
-    <Route path='경로C' element={<컴포넌트3 />} />
-  </Route>
+import {Routes, Route} from "react-router-dom";
 
-</Routes>
+import Home from './components/Home';
+import TopNavi from './components/TopNavi';
+import NotFound from './components/NotFound';
+
+function App() {
+  return(<>
+    <TopNavi></TopNavi>
+    <Routes>
+      <Route path='/' element={<Home />}></Route>
+      <Route path='*' element={<NotFound />} />
+    </Routes>
+  </>)
+}
+
+export default App;
