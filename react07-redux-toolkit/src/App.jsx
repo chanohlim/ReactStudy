394da0758c36883d63c8be59338b79247512ac3f
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-06-06 08:12:12 +0000

    .

---
+++ react07-redux-toolkit/src/App.jsx
@@ -0,0 +1,29 @@
+import {Routes, Route} from 'react-router-dom';
+import TopNavi from './components/TopNavi';
+
+// import { Provider as BasicProvider } from 'react-redux';
+// import { store as basicStore } from './exam1/store';
+// import ReduxBasicApp from './exam1/ReduxBasicApp';
+
+// import { Provider as TodoProvider } from 'react-redux';
+// import { store as todoStore } from './exam1/store';
+// import TodoApp from './exam2/TodoApp';
+
+function App() {
+  return(<>
+    <TopNavi></TopNavi>
+    <Routes>
+      {/* <Route path='/' element={<BasicProvider store={basicStore}>
+        <ReduxBasicApp />
+      </BasicProvider>} /> */}
+      {/* <Route path='/ReduxBasicApp' element={<BasicProvider store={basicStore}>
+        <ReduxBasicApp></ReduxBasicApp>
+      </BasicProvider>}></Route> */}
+      {/* <Route path='/TodoApp' element={<TodoProvider store={todoStore}>
+        <TodoApp></TodoApp>
+      </TodoProvider>}></Route> */}
+    </Routes>
+  </>)
+}
+
+export default App;
