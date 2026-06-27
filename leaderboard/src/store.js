66b5506e0a4e4640ac2f95c9c16c492585449d76
Chan Oh Lim <153146750+chanohlim@users.noreply.github.com>
2026-06-27 11:43:17 +0000

    .

--- leaderboard/src/store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit';
-import distractionsReducer from './features/distractions/distractionsSlice';
-
-export const store = configureStore({
-  reducer: {
-    distractions: distractionsReducer,
-  },
-});
